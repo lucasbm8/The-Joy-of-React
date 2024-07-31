84cc2036ad8ca5a4cc434ac86df3426ddf19a2f7
Lucas Martins <lmartins@tendencias.com.br>
2024-07-31 15:06:38 -0300

    chore: Refactor Guess component to use Cell component for rendering individual cells in GuessList

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,17 +3,23 @@ import React from "react";
 import { range } from "../../utils";
 
 import { checkGuess } from "../../game-helpers.js";
+
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : `cell`;
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value, answer }) {
   const result = checkGuess(value, answer);
-
-  console.log({ result });
 
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
